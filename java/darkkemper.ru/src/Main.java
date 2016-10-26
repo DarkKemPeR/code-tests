f2cf0fdf610f1afa60ad1eafc96ea4a532482a7d
Eugene Tatarinov <darkkemper@gmail.com>
2016-10-26 17:06:19 +0300

    test git

--- java/darkkemper.ru/src/Main.java
+++ java/darkkemper.ru/src/Main.java
@@ -3,22 +3,24 @@ import Observer.Variant2;
 /**
  * Created by darkkemper on 26.10.16.
  */
-
-/**
- * Example of realisation design pattern "Observer"
- */
 public class Main
 {
     public static void main(String[] args)
     {
         /**
-         * First without implementing interface
+         * <b>If you need to test one of examples below just uncomment them</b>
          */
-        new Variant1(20);
 
         /**
+         * Example of realisation design pattern "Observer"
+         * First without implementing interface
+         */
+//        new Variant1(20);
+
+        /**
+         * Example of realisation design pattern "Observer"
          * Second with implementing
          */
-        new Variant2(20);
+//        new Variant2(20);
     }
 }
